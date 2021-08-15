8aec78518bb6063bfd9d13671cd61b82042cde2a
Aayush Bhatnagar <aayushbhatnagar1999@gmail.com>
2021-08-15 22:56:59 +0530

    working

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+var c = 10;
+var d = 11;
+console.log(add(c, d));
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add(a: number,b: number){
+    return a+b;
+}
+
+const c=10;
+const d=11;
+
+console.log(add(c,d))
